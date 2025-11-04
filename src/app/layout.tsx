28c28d1dbf1f46896bd0eb95066d91f6700149fa
Flavio Andre <flavioandre@gmail.com>
2025-11-03 22:17:52 -0300

    Reorganização da estrutura: movido app para src/ e configurado alias @ - Aula 02

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Nunito} from "next/font/google";
+import "@/styles/globals.css";
+
+const nunito = Nunito({
+  variable: "--font-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "WiseWra",
+  description: "WiseWra app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${nunito.variable} antialiased font-sans`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
